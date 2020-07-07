f849103405c63cee30f1863c8766b2743825dd0b
Alessandro Cordero Salazar (PwC) <acorderosalazar@hogaresconectados.net>
2020-07-07 08:50:08 -0600

    Correcciones facilitadoras y aclaradoras

--- app.py
+++ app.py
@@ -1,45 +1,107 @@
+"""
+# Aplicación foliadora de archivos PDF.
+Usa los siguientes directorios:
+
+ - `pdfs-sin-foliar/`: archivos originales pendientes de foliar
+ - `pdfs-procesados/`: archivos originales ya foliados
+ - `pdfs-foliados/`: archivos nuevos con páginas foliadas
+
+"""
 import reportlab
 from reportlab.lib.units import mm
 from reportlab.pdfgen import canvas
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-def obtiene_secuencia():
-    texto = "Ninguno - 1"
+
+def secuencia_obtiene():
+    """
+        Obtiene la secuencia almancenada en el archivo `foliado.txt`,
+        devuelve un par ordenado: (clase,secuencia).
+    """
+    clase_docs = "Ninguno"
+    secuencia = 1    
     try:
         file1 = open("foliado.txt","r") 
         texto = file1.readline()
     except:
+        texto = f"{clase_docs} {secuencia}"
         print("Archivo foliado.txt no encontrado o con contenido inválido")
-    return texto
+    if " - " in texto:
+        clase_docs=texto.split(" - ")[0]
+        secuencia=int(texto.split(" - ")[1])
+    elif " " in texto:
+        clase_docs=texto.split(" ")[0]
+        secuencia=int(texto.split(" ")[1])
 
-def modifica_pdf(num, tmp, clase_docs, secuencia):
-    c = canvas.Canvas(tmp)
+    return clase_docs,secuencia
+
+
+
+def secuencia_actualiza(clase_docs:str,secuencia:int):
+    """
+        Escribe el siguiente sello en el archivo `foliado.txt`.
+    """
+    try:
+        with open('foliado.txt', 'w') as file1:
+            file1.write(f"{clase_docs} - {secuencia}")
+            print(f"siguiente sello: {clase_docs} - {secuencia}")
+    except:
+        print("Archivo foliado.txt no encontrado o con contenido inválido")
+
+
+
+def pdf_imprime_sellos(num, tmp, clase_docs, secuencia):
+    """
+        Imprime los sellos con la secuencia en hojas limpias, usa archivo temporal.
+    """
+    lienzo = canvas.Canvas(tmp)
     for i in range(0,num): 
-        c.rotate(90)
-        c.setFillColorRGB(0,0,0.77)
-        c.drawString((5)*mm, (-5)*mm, clase_docs + " - " + str(i+secuencia))
-        c.showPage()
-    c.save()
+        # lienzo.rotate(90)
+        lienzo.setFont("Helvetica", 20)
+        lienzo.setFillColorRGB(0, 0, 0.77)
+        # lienzo.drawString((5)*mm, (-5)*mm, clase_docs + " - " + str(i+secuencia)) # izquierda abajo paralelo-lado
+        # lienzo.drawString(0, 0, clase_docs + " - " + str(i+secuencia)) # izquierda abajo paralelo-abajo
+        lienzo.setFont("Helvetica",20)
+        lienzo.rotate(90)
+        lienzo.drawString((50)*mm, (-150)*mm, clase_docs + " - " + str(i+secuencia))
+        lienzo.showPage()
+    lienzo.save()
     return
 
+
+
+def pdf_mueve(nombre):
+    """
+        Mueve el archivo procesado fuera del directorio de trabajo en la carpeta
+        `pdfs-procesados`.
+    """
+    import os
+    from shutil import move
+    if not os.path.isdir("pdfs-procesados/"):
+        os.mkdir("pdfs-procesados/")
+    path1 = os.path.join("pdfs-sin-foliar", nombre)
+    path2 = os.path.join("pdfs-procesados", nombre)
+    move(path1, path2)
+
+
+
 if __name__ == "__main__":
-    import sys, os
+    import os
     tmp = "__tmp.pdf"
     
     output = PdfFileWriter()
 
-    clase_docs=obtiene_secuencia()
-    secuencia=int(clase_docs.split(" - ")[1])
-    clase_docs=clase_docs.split(" - ")[0]
-
-    for filename in os.listdir("pdfs-sin-foliar"):
+    (clase_docs,secuencia) = secuencia_obtiene()
+    lista_archivos=os.listdir("pdfs-sin-foliar")
+    for filename in lista_archivos:
         base = os.path.basename(filename)
-        path = os.path.join('pdfs-sin-foliar', filename)
-        if filename.endswith('.pdf'):            
+        path = os.path.join("pdfs-sin-foliar", filename)
+        if filename.endswith('.pdf'):
             with open(path, "rb") as f:
+                print(f"inicia con archivo {filename}")
                 pdf = PdfFileReader(f,strict=False)
                 n = pdf.getNumPages()
-                modifica_pdf(n,tmp,clase_docs,secuencia)
+                pdf_imprime_sellos(n,tmp,clase_docs,secuencia)
                 secuencia=secuencia+n
 
                 if not os.path.isdir("pdfs-foliados/"):
@@ -50,9 +112,8 @@ if __name__ == "__main__":
                     with open(newpath, "wb") as f:
                         output.write(f)
                     output = PdfFileWriter()
-                    for p in range(n):                                                    
-                        # sys.stdout.write("\rpage: %d of %d"%(p, n))
-                        print("page: %d of %d"%(p, n))
+                    for p in range(n):
+                        print(f"hoja: {p+1} de {n}")
                         page = pdf.getPage(p)
                         numberLayer = numberPdf.getPage(p)
                         
@@ -63,3 +124,13 @@ if __name__ == "__main__":
                         with open(newpath, "wb") as f:
                             output.write(f)            
                 os.remove(tmp)
+            pdf_mueve(filename)
+            print(f"termina con archivo {filename}")
+
+    if len(lista_archivos)>0:
+        secuencia_actualiza(clase_docs, secuencia)        
+        print("Fin del proceso")
+    else:
+        print("no hubo archivos para foliar")
+
+    
